7607be1adbdfa1bb834f17165dbe19c54a499a69
reettginotra <reetginotraa@gmail.com>
2026-06-28 12:33:32 +0530

    new problem added/100

---
+++ Problems/validate-binary-search-tree.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+
+        def dfs(node, low, high):
+            if not node:
+                return True
+
+            if node.val <= low or node.val >= high:
+                return False
+
+            return dfs(node.left, low, node.val) and dfs(node.right, node.val, high)
+
+        return dfs(root, float("-inf"), float("inf"))
