b12e4ef839846513d6447be462fc47e95f28926a
reettginotra <reetginotraa@gmail.com>
2026-07-13 11:03:57 +0530

    Add leetcode daily challenge solution

---
+++ Problems/1291-sequential-digits.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// LeetCode solution starts from here
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        string s = "123456789";
+        vector<int> ans;
+
+        int l = to_string(low).size();
+        int r = to_string(high).size();
+
+        for (int len = l; len <= r; len++) {
+            for (int i = 0; i + len <= 9; i++) {
+                int num = stoi(s.substr(i, len));
+                if (num >= low && num <= high)
+                    ans.push_back(num);
+            }
+        }
+
+        return ans;
+    }
+};
