17e65b949d6b0fba4faa7d19872709de68fb89d7
reettginotra <reetginotraa@gmail.com>
2026-07-23 11:05:55 +0530

    Add leetcode daily challenge solution

---
+++ Problems/3513-number-of-unique-xor-triplets-i.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// LeetCode solution starts from here
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 2) return n;
+        int p = 1;
+        while (p <= n) p <<= 1;
+        return p;
+    }
+};
