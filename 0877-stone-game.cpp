a09e57d0b4c7449c778035e1a306943fbc9d638e
reettginotra <reetginotraa@gmail.com>
2026-08-02 19:13:22 +0530

    Add daily challenge leetcode solution

---
+++ 0877-stone-game.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// LeetCode solution starts from here
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
