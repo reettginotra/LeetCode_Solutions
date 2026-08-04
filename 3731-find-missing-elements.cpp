b7b5054cb597befcdea8b99b86ae6074217ba70d
reettginotra <reetginotraa@gmail.com>
2026-08-04 10:25:07 +0530

    Add daily challenge leetcode solution

---
+++ 3731-find-missing-elements.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// LeetCode solution starts from here
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int mn = *min_element(nums.begin(), nums.end());
+        int mx = *max_element(nums.begin(), nums.end());
+
+        unordered_set<int> st(nums.begin(), nums.end());
+        vector<int> ans;
+
+        for (int x = mn; x <= mx; x++)
+            if (!st.count(x))
+                ans.push_back(x);
+
+        return ans;
+    }
+};
