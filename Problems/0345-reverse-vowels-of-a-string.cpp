1115124604030526a0cbe9df3213cd7dff2c901c
reettginotra <reetginotraa@gmail.com>
2026-08-21 10:48:41 +0530

    0345 leetcode sol in cpp

---
+++ Problems/0345-reverse-vowels-of-a-string.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// LeetCode solution starts from here
+class Solution {
+public:
+    string reverseVowels(string s) {
+        string vowels = "aeiouAEIOU";
+        int i = 0, j = s.size() - 1;
+
+        while (i < j) {
+            while (i < j && vowels.find(s[i]) == string::npos)
+                i++;
+
+            while (i < j && vowels.find(s[j]) == string::npos)
+                j--;
+
+            swap(s[i], s[j]);
+            i++;
+            j--;
+        }
+
+        return s;
+    }
+};
