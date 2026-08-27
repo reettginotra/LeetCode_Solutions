2323e4546e75940bce496d349f314a8f6de4ba7f
reettginotra <reetginotraa@gmail.com>
2026-08-27 11:35:58 +0530

    0384 leetocode sol in cpp

--- Problems/0384-shuffle-an-array.py
+++ Problems/0384-shuffle-an-array.py
@@ -1,27 +1,50 @@
-import random
+#include <bits/stdc++.h>
+using namespace std;
 
-class Solution(object):
+// LeetCode solution starts from here
 
-    def __init__(self, nums):
-        """
-        :type nums: List[int]
-        """
-        self.original = nums[:]
+class Solution {
+    vector<int> original;
 
-    def reset(self):
-        """
-        :rtype: List[int]
-        """
-        return self.original
+public:
+    Solution(vector<int>& nums) {
+        original = nums;
+    }
 
-    def shuffle(self):
-        """
-        :rtype: List[int]
-        """
-        arr = self.original[:]
+    vector<int> reset() {
+        return original;
+    }
 
-        for i in range(len(arr) - 1, 0, -1):
-            j = random.randint(0, i)
-            arr[i], arr[j] = arr[j], arr[i]
+    vector<int> shuffle() {
+        vector<int> arr = original;
 
-        return arr
+        for (int i = arr.size() - 1; i > 0; i--) {
+            int j = rand() % (i + 1);
+            swap(arr[i], arr[j]);
+        }
+
+        return arr;
+    }
+};
+
+int main() {
+    vector<int> nums = {1, 2, 3};
+
+    Solution sol(nums);
+
+    vector<int> shuffled = sol.shuffle();
+
+    for (int x : shuffled)
+        cout << x << " ";
+
+    cout << "\n";
+
+    vector<int> reset = sol.reset();
+
+    for (int x : reset)
+        cout << x << " ";
+
+    cout << "\n";
+
+    return 0;
+}
