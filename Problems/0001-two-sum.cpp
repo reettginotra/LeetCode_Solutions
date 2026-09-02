6bb18177751432daf30e30be505fee7af4da1b5c
reettginotra <reetginotraa@gmail.com>
2026-09-02 09:47:35 +0530

    0001 leetcode solution

---
+++ Problems/0001-two-sum.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// LeetCode solution starts from here
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> lookup;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (lookup.count(target - nums[i])) {
+                return {lookup[target - nums[i]], i};
+            }
+            lookup[nums[i]] = i;
+        }
+        return {};
+    }
+};
