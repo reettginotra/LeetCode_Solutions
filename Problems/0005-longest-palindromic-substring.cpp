0512868b34b749a1be8bbfc17d34d75ebfa22d32
reettginotra <reetginotraa@gmail.com>
2026-09-06 12:17:04 +0530

    0005 leetcode solution in cpp

---
+++ Problems/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// LeetCode solution starts from here
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        int start = 0, maxLen = 1;
+
+        auto expand = [&](int left, int right) {
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                if (right - left + 1 > maxLen) {
+                    start = left;
+                    maxLen = right - left + 1;
+                }
+                left--;
+                right++;
+            }
+        };
+
+        for (int i = 0; i < n; i++) {
+            // Odd-length palindrome
+            expand(i, i);
+
+            // Even-length palindrome
+            expand(i, i + 1);
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
