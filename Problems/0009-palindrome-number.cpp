919d88aef807f3e6220248fcc61519004776a198
reettginotra <reetginotraa@gmail.com>
2026-09-10 21:45:45 +0530

    0009 leetcode sol in cpp

---
+++ Problems/0009-palindrome-number.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// LeetCode solution starts from here
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int temp = x;
+        int reversed = 0;
+        while (temp != 0) {
+            if (isOverflow(reversed, temp % 10)) {
+                return false;
+            }
+            reversed = reversed * 10 + temp % 10;
+            temp = temp / 10;
+        }
+        return reversed == x;
+    }
+
+private:
+    bool isOverflow(int q, int r) {
+        static const int max_q = numeric_limits<int>::max() / 10;
+        static const int max_r = numeric_limits<int>::max() % 10;
+        return (q > max_q) || (q == max_q && r > max_r);
+    }
+};
