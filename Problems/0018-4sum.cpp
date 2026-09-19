e20cc7fee5410b248130abd0f00ce6285677cb82
reettginotra <reetginotraa@gmail.com>
2026-09-19 15:57:18 +0530

    0018 leetcode solution in cpp

---
+++ Problems/0018-4sum.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// LeetCode solution starts from hereclass Solution 
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> result;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                
+                long long total = (long long)target - nums[i] - nums[j];
+                int left = j + 1;
+                int right = n - 1;
+                while (left < right) {
+                    long long sum = (long long)nums[left] + nums[right];
+                    
+                    if (sum == total) {
+                        result.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        right--;
+                        left++;
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    } else if (sum > total) {
+                        right--;
+                    } else {
+                        left++;
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+};
