0afd59738a7330143b2b77bb35807f7c358f5527
reettginotra <reetginotraa@gmail.com>
2026-09-24 11:35:57 +0530

    0023 leetcode solution in cpp

---
+++ Problems/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+// LeetCode solution starts from hereclass Solution 
+class Solution {
+public:
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        if (lists.empty()) {
+            return nullptr;
+        }
+
+        int left = 0, right = lists.size() - 1;
+        while (right > 0) {
+            lists[left] = mergeTwoLists(lists[left], lists[right]);
+            ++left;
+            --right;
+            if (left >= right) {
+                left = 0;
+            }
+        }
+        return lists[0];
+    }
+    
+private:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode dummy{0};
+        auto curr = &dummy;
+
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                curr->next = l1;
+                l1 = l1->next;
+            } else {
+                curr->next = l2;
+                l2 = l2->next;
+            }
+            curr = curr->next;
+        }
+        curr->next = l1 ? l1 : l2;
+
+        return dummy.next;
+    }
+};
+
