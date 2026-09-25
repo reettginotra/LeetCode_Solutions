5a89bfeb9b3a1f5a5323b23f5f2563f650005e92
reettginotra <reetginotraa@gmail.com>
2026-09-25 11:48:19 +0530

    0024 leetcode solution in cpp

---
+++ Problems/0024-swap-nodes-in-pairs.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+// LeetCode solution starts from hereclass Solution 
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode dummy{0};
+        dummy.next = head;
+        auto curr = &dummy;
+        while (curr->next && curr->next->next) {
+            auto next_one = curr->next;
+            auto next_two = next_one->next;
+            auto next_three = next_two->next;
+            curr->next = next_two;
+            next_two->next = next_one;
+            next_one->next = next_three;
+            curr = next_one;
+        }
+        return dummy.next; 
+    }
+};
